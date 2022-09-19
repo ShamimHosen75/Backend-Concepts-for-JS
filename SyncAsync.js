aa08bbbfc98592ff9cc8b47978acdb1410c7934b
ShamimHosen75 <developer.shamimhosen@gmail.com>
2022-09-19 22:01:42 +0600

    Call back hell Async task

--- SyncAsync.js
+++ SyncAsync.js
@@ -3,4 +3,16 @@ const colors = required('colors');
 // Start
 
 console.log("Welcome to ACC!");
-console.log("I'm excited!".red);
+setTimeout(() =>{
+  const user = {id: 1};
+
+  setTimeout(() => {
+    const product = [{}, {}, {}];
+
+    setTimeout(() => {
+      const prices = [];
+    },1000)
+  }, 500);
+}, 1000);
+
+console.log("I'm excited!"); 
