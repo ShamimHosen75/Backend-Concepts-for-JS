1a311e7f3000bc821bb9fd990308ffe7eb732759
ShamimHosen75 <developer.shamimhosen@gmail.com>
2022-11-06 02:47:41 +0600

    error handler

--- Error.js
+++ Error.js
@@ -1,7 +1,27 @@
 async function getData() {
-  
+  try{
+    // undefined.find();
+    const emailError = new Error("Email Already exists!");
+    throw emailError;
+  } catch (error){
+    errorHandler(error);
+  }
 }
 
 getData();
+
+function errorHandler (error){
+  const {name, message, stack} = error;
+
+  console.log(name, message, stack);
+
+  // logger.error({
+  //   name,
+  //   message,
+  //   stack,
+  // });
+
+  // console.log("Internal Server Error!");
+}
 
 console.log("Done!");
